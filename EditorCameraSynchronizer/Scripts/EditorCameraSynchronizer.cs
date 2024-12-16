e33e1b113b2296d5d276fcd0c6d7e524ad860f1e
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2024-12-16 12:34:25 +0900

    Add: EditorCameraSynchronizerを追加

---
+++ EditorCameraSynchronizer/Scripts/EditorCameraSynchronizer.cs
@@ -0,0 +1,37 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+namespace UTecStack
+{
+    [RequireComponent(typeof(Camera))]
+    public class EditorCameraSynchronizer : MonoBehaviour
+    {
+        private Camera gameCamera;
+        private Camera sceneCamera;
+
+        private void Start()
+        {
+            gameCamera = GetComponent<Camera>();
+            if (SceneView.lastActiveSceneView != null)
+            {
+                sceneCamera = SceneView.lastActiveSceneView.camera;
+            }
+        }
+
+        void Update()
+        {
+            // シーンビューが無ければ実行しない
+            if (SceneView.lastActiveSceneView == null) { return; }
+            if (SceneView.lastActiveSceneView.camera != sceneCamera)
+            {
+                // カメラのインスタンスが異なれば新しく代入
+                sceneCamera = SceneView.lastActiveSceneView.camera;
+            }
+
+            gameCamera.transform.position = sceneCamera.transform.position;
+            gameCamera.transform.rotation = sceneCamera.transform.rotation;
+            gameCamera.fieldOfView = sceneCamera.fieldOfView;
+        }
+    }
+}
+#endif
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # UTecStack
+効率を高めるためのツールや、汎用性の高い機能をまとめておくリポジトリ。
+
+# EditorCameraSynchronizer
+[単独リポジトリ](https://github.com/nkc20236039/EditorCameraSynchronizer)
+シーンビューで見ている画面とゲームビューのカメラの画面を同期します。
+
+## 使い方
+Prefabをヒエラルキーに配置するか、任意のカメラに「EditorCameraSynchronizer」スクリプトをアタッチしてください。  
+PrefabのデフォルトはDisplay2に描画するように設定されています。
